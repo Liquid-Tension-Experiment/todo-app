2893bc9691125ba6b4ebedd708054e5edf69c528
Liquid-Tension-Experiment <98864432+Liquid-Tension-Experiment@users.noreply.github.com>
2025-03-15 14:57:37 -0500

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+// import exampleImage from "./img/example.png";
+
+// const image = document.createElement("img");
+// image.src = exampleImage
+// import { displayHomePage } from "./home.js";
+import "../styles.css";
