398576f6847cf1560f7b63835f7bffd1dc80b97b
Joe Eng <engjoe1992@gmail.com>
2025-03-15 19:49:54 -0500

    Implement displaying single task in right column

--- src/js/display.js
+++ src/js/display.js
@@ -18,7 +18,7 @@ export default class Display {
             taskSection.appendChild(taskRow);
 
             let icon = document.createElement("div");
-            let priorityLevel = this.#getPriorityString(task);
+            let priorityLevel = this.#getPriorityClassName(task);
             icon.classList.add("priority-icon", priorityLevel);
             taskRow.appendChild(icon);
 
@@ -34,10 +34,83 @@ export default class Display {
 
         }
     }
-    displaySingleTask(){
+    displaySingleTask(task){
+        const rightCol = document.querySelector('.main-right-col');
+        this.#emptyContent(rightCol);
+
+        let taskDisplay = document.createElement("div");
+        taskDisplay.classList.add("task-display");
+        rightCol.appendChild(taskDisplay);
+
+        let row = document.createElement("div");
+        row.classList.add("row-one");
+        taskDisplay.appendChild(row);
+
+        let title = document.createElement("div");
+        title.classList.add("task-title");
+        title.textContent = task.title;
+        row.appendChild(title);
+
+        let pText = document.createElement("div");
+        title.classList.add("priority-text");
+        pText.textContent = this.#getPriorityString(task);
+        row.append(pText);
+
+        let icon = document.createElement("div");
+        let priorityLevel = this.#getPriorityClassName(task);
+        icon.classList.add("priority-icon", priorityLevel);
+        row.appendChild(icon);
+
+        row = document.createElement("div");
+        row.classList.add("row-two");
+        taskDisplay.appendChild(row);
+
+        let text = document.createElement("div");
+        text.textContent = `Project: ${task.project}`;
+        row.append(text);
+
+        row = document.createElement("div");
+        row.classList.add("row-three");
+        taskDisplay.appendChild(row);
+
+        text = document.createElement("div");
+        text.textContent = `Description`;
+        row.append(text);
+
+        text = document.createElement("div");
+        text.textContent = task.description;
+        row.append(text);
+
+        row = document.createElement("div");
+        row.classList.add("task-button-row");
+        taskDisplay.appendChild(row);
+
+        let button = document.createElement("button");
+        button.type = "button";
+        button.id = "btn-edit"
+        button.text = "Edit";
+        row.appendChild(button);
+
+        button = document.createElement("button");
+        button.type = "button";
+        button.id = "btn-remove"
+        button.text = "Remove";
+        row.appendChild(button);
 
     }
     #getPriorityString(element)
+    {
+        if(element.priority == 'high'){
+            return 'High';
+        }else if(element.priority == 'med'){
+            return 'Medium';
+        }else if(element.priority == 'low'){
+            return 'Low';
+        }else{
+            return "error";
+        }
+    }
+    #getPriorityClassName(element)
     {
         if(element.priority == 'high'){
             return 'priority-high';
--- src/js/index.js
+++ src/js/index.js
@@ -13,16 +13,25 @@ const tasks = []
 let mytodo = new ToDo('Change oil',
     'Dodge Dakota needs an oil change.',
     '7/7/2025',
-    'med',
+    'low',
 )
 let mytodo2 = new ToDo('Buy Groceries',
     "I am quite hungry. I should pick up fruit, meat, vegetables and maybe dog food too",
     '3/17/2025',
+    'med',
+)
+let mytodo3 = new ToDo('Walk the dog',
+    "This dog sure is getting fat.",
+    '3/15/2025',
     'high',
 )
-tasks.push(mytodo)
-tasks.push(mytodo2)
+tasks.push(mytodo);
+tasks.push(mytodo2);
+tasks.push(mytodo3);
 
 const display = new Display();
+window.display = display;
+window.tasks = tasks;
 display.displayTasks(tasks);
+display.displaySingleTask(tasks[2]);
 console.log(mytodo);
